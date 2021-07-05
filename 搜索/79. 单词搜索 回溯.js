b24d1d54ade14daeb9b4ba6d282a1b32ae9ac034
zhangqianming <zhangqianming@xiaohongshu.com>
2021-07-05 19:31:59 +0800

    单词回溯

---
+++ 搜索/79. 单词搜索 回溯.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+  
+};
+
+console.log(exist(
+  [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+  ],
+  "ABCCED"
+));
