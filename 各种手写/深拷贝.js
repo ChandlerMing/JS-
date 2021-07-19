91c9b128d4ec9bc9000dc2e8e2e20e3303eae735
ChandlerMing <2488428181@qq.com>
2021-07-19 09:15:02 +0800

    手写题

---
+++ 各种手写/深拷贝.js
@@ -0,0 +1,16 @@
+
+function clone(target, map = new Map()) {
+  if (typeof target === 'object') {
+      let cloneTarget = Array.isArray(target) ? [] : {};
+      if (map.get(target)) {
+          return map.get(target);
+      }
+      map.set(target, cloneTarget);
+      for (const key in target) {
+          cloneTarget[key] = clone(target[key], map);
+      }
+      return cloneTarget;
+  } else {
+      return target;
+  }
+};
