8e60c0e6d5f441a273a88180610aa58389a6872c
ChandlerMing <2488428181@qq.com>
2021-08-12 00:12:20 +0800

    reduce实现map

---
+++ 各种手写/reduce实现map.js
@@ -0,0 +1,12 @@
+Array.prototype._map = function (fn, callbackThis = null) {
+  return this.reduce((acc, cur, index, array) => acc.concat(fn.call(callbackThis, cur, index, array)), [])
+}
+
+let a = [1, 2, 3, 4, 5];
+
+console.log(a.map(el => el * 2));
+
+console.log(a._map(el => el * 2));
+
+setInterval(()=>{}, 10000);
+
