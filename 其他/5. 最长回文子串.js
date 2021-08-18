c21f9ca4f20bfd35184562002655f728e8a4c49f
ChandlerMing <2488428181@qq.com>
2021-08-18 11:38:12 +0800

    最长回文子串

---
+++ 其他/5. 最长回文子串.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  let arr = ['#', ...s.split('').join('#'), '#'];
+  let maxLenght = 0;
+  let resRange = [0, 0];
+  for (let i = 0; i < arr.length; i++) {
+    let left = i - 1, right = i + 1;
+    let cur = 0;
+    while (left >= 0 && right < arr.length) {
+      if (arr[left] === arr[right]) {
+        cur++;
+        left--;
+        right++;
+      } else {
+        break;
+      }
+    }
+    if (cur > maxLenght) {
+      maxLenght = cur;
+      resRange = [left + 1, right - 1 + 1] // 'right - 1' offsets the final 'right++'. 'right + 1' adapt the slice index.
+    }
+  }
+  return arr.slice(resRange[0], resRange[1]).filter(el => el != '#').join('');;
+};
+
+console.log(longestPalindrome('cbbd'))
