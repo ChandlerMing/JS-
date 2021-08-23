29be6e37006c278641b0b80dd9bb3086a1fd65f9
ChandlerMing <2488428181@qq.com>
2021-08-23 23:18:26 +0800

    双路快排

--- 其他/912. 排序数组.js
+++ 其他/912. 排序数组.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// 最简单快排
 var sortArray = function (nums) {
   if (nums.length < 2) {
     return nums;
@@ -19,8 +21,40 @@ var sortArray = function (nums) {
   return sortArray(left).concat([pivot], sortArray(right));
 };
 
+// 双路快排
 var sortArray = function (nums, left = 0, right = nums.length - 1) {
-
+  function swap(arr, left, right) {
+    let tmp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = tmp;
+  }
+  if (left > right) {
+    return;
+  }
+  let mid = ~~(left + (right - left) / 2), i = left + 1, j = right;
+  let pivot = nums[mid];
+  swap(nums, left, mid);
+  while (true) {
+    while (i <= right && nums[i] <= pivot) {
+      i++;
+    }
+    while (j >= left + 1 && nums[j] >= pivot) {
+      j--;
+    }
+    if (i >= j) {
+      break;
+    }
+    swap(nums, i, j)
+    i++;
+    j--;
+  }
+  swap(nums, left, j)
+  sortArray(nums, left, j - 1)
+  sortArray(nums, j + 1, right)
+  return nums;
 };
 
-console.log(sortArray([3,2,1,5]));
+// 三路快排
+// Todo
+
+console.log(sortArray([-1, 2, -8, -10]));
