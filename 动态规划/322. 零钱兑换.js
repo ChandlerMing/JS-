d0c420d59aac0838b3c03f395ca5dca7803b1c0f
ChandlerMing <2488428181@qq.com>
2021-08-31 01:52:33 +0800

    update

---
+++ 动态规划/322. 零钱兑换.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  let dp = new Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (i >= coins[j]) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+  }
+  return dp[amount] > amount ? -1 : dp[amount];
+};
+
+console.log(coinChange([1, 2, 5], 11));
