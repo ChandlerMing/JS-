2dbfe190c9e3a14872c136910ad92cf1efe6c470
ChandlerMing <2488428181@qq.com>
2021-09-08 23:14:39 +0800

    update

---
+++ 原生开发/jsonpServer.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var urllib = require('url');
+
+var port = 8080;
+var data = {'data':'world'};
+
+http.createServer(function(req,res){
+    var params = urllib.parse(req.url,true);
+    if(params.query.callback){
+        console.log(params.query.callback);
+        //jsonp
+        var str = params.query.callback + '(' + JSON.stringify(data) + ')';
+        res.end(str);
+    } else {
+        res.end();
+    }
+    
+}).listen(port,function(){
+    console.log('jsonp server is on');
+});
