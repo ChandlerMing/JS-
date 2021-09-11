c6e1fa7f0d799c3a49eecce59fd6376e5cf9e7e6
ChandlerMing <2488428181@qq.com>
2021-09-11 10:57:21 +0800

    update

--- 二分/33. 搜索旋转排序数组.js
+++ 二分/33. 搜索旋转排序数组.js
@@ -3,30 +3,30 @@
  * @param {number} target
  * @return {number}
  */
- var search = function(nums, target) {
-  let l = 0 , r = nums.length - 1, res = -1;
-  while ( l <= r ) {
-      const mid = int( l + ( ( r - l ) >> 1 ) )
-      if ( nums[mid] === target ) {
-          return mid;
-      }
-      if ( nums[mid] >= nums[l] ) {
-          if ( target >= nums[l] && target <= nums[mid] ) {
-              r = mid - 1;
-          } else {
-              l = mid + 1;
-          }
+var search = function (nums, target) {
+  let l = 0, r = nums.length - 1, res = -1;
+  while (l <= r) {
+    const mid = int(l + ((r - l) >> 1))
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] >= nums[l]) {
+      if (target >= nums[l] && target <= nums[mid]) {
+        r = mid - 1;
       } else {
-          if ( target >= nums[mid] && target <= nums[r] ) {
-              l = mid + 1;
-          } else {
-              r = mid - 1;
-          }
+        l = mid + 1;
       }
+    } else {
+      if (target >= nums[mid] && target <= nums[r]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
   }
   return res;
 };
 
-var int = function(num) {
+var int = function (num) {
   return Math.floor(num)
 }
