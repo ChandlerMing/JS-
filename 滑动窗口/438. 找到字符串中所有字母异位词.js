9b199188901eb2a61d61ae49989a29b2c9463aab
ChandlerMing <2488428181@qq.com>
2021-09-13 01:33:32 +0800

    update

--- 滑动窗口/438. 找到字符串中所有字母异位词.js
+++ 滑动窗口/438. 找到字符串中所有字母异位词.js
@@ -6,6 +6,9 @@
 var findAnagrams = function (s, p) {
   let need = new Map(), window = new Map();
   let left = 0, right = 0, valid = 0, res = [];
+  for (let char of p) {
+    need.set(char, ~~need.get(char) + 1);
+  }
   while (right < s.length) {
     let inWindow = s[right];
     if (need.has(inWindow)) {
@@ -14,11 +17,22 @@ var findAnagrams = function (s, p) {
         valid++;
       }
     }
-    while (valid === p.length) {
-      res.push(left);
-      while () {
-
+    while (valid === need.size) {
+      if (right - left + 1 === p.length) {
+        res.push(left);
       }
+      let outWindow = s[left];
+      if (need.has(outWindow)) {
+        if (window.get(outWindow) === need.get(outWindow)) {
+          valid--;
+        }
+        window.set(outWindow, ~~window.get(outWindow) - 1)
+      }
+      left++;
     }
+    right++;
   }
+  return res;
 };
+
+console.log(findAnagrams("cbaebabacd", "abc"));
