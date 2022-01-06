3bb3501bd823b9214ae268e27c08e2f65601c010
ChandlerMing <2488428181@qq.com>
2022-01-07 01:29:26 +0800

    update

--- 动态规划/10. 正则表达式匹配.js
+++ 动态规划/10. 正则表达式匹配.js
@@ -4,7 +4,7 @@
  * @param {string} p
  * @return {boolean}
  */
-let isMatch = function (s, p) {
+var isMatch = function (s, p) {
   const sL = s.length;
   const pL = p.length;
   function dp(i, j) {
@@ -36,3 +36,50 @@ let isMatch = function (s, p) {
   }
   return dp(0, 0);
 };
+
+/**
+ * Use memory
+ */
+var isMatch = function (s, p) {
+  const sL = s.length;
+  const pL = p.length;
+  const memo = new Map();
+  function dp(i, j) {
+    if (j === pL) {
+      return i === sL;
+    }
+    if (i === sL) {
+      if ((pL - j) % 2 !== 0) {
+        return false;
+      }
+      for (; j < pL; j += 2) {
+        if (p[j + 1] !== '*') {
+          return false;
+        }
+      }
+      return true;
+    }
+    const key = i + '' + j;
+    if (memo.has(key)) {
+      return memo.get(key);
+    }
+    let res = false;
+    if (s[i] === p[j] || p[j] === '.') {
+      if (j < pL - 1 && p[j + 1] === '*') {
+        res = dp(i + 1, j) || dp(i, j + 2);
+      } else {
+        res = dp(i + 1, j + 1);
+      }
+    } else {
+      if (j < pL - 1 && p[j + 1] === '*') {
+        res = dp(i, j + 2);
+      }
+    }
+    memo.set(key, res);
+    return res;
+  }
+  return dp(0, 0);
+};
+
+
+console.log(isMatch('aa', 'a*'))
