40fae62cc201909a1a142f207c3a37f84c26a896
ChandlerMing <2488428181@qq.com>
2022-02-24 02:57:58 +0800

    dp

--- 动态规划/518. 零钱兑换 II.js
+++ 动态规划/518. 零钱兑换 II.js
@@ -36,4 +36,16 @@ var change = function (amount, coins) {
   return dp[amount];
 };
 
+var change = function (amount, coins) {
+  let n = coins.length;
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = coins[i]; j <= amount; j++) {
+      dp[j] = dp[j] + dp[j - coins[i]];
+    }
+  }
+  return dp[amount];
+};
+
 console.log(change(5, [1, 2, 5]));
