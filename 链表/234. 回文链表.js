7f92cddaaf99774934fc8128b0257a474466d2e8
ChandlerMing <2488428181@qq.com>
2022-05-16 21:01:08 +0800

    update

--- 链表/234. 回文链表.js
+++ 链表/234. 回文链表.js
@@ -40,5 +40,50 @@ var isPalindrome = function (head) {
   }
   return dfs(head);
 };
-// 
+// Fast & Slow Pointer
+var isPalindrome = function (head) {
+  let left = head;
+  function dfs(node) {
+    if (!node) {
+      return true;
+    }
+    let res = dfs(node.next);
+    res = res && (node.val === left.val);
+    left = left.next;
+    return res;
+  }
+  return dfs(head);
+};
+var isPalindrome = function (head) {
+  // Loacate the mid of the linked list
+  let slow = head, fast = head;
+  while (fast.next !== null && fast.next.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  if (fast !== null) {
+    slow = slow.next;
+  }
+  function reverseList(head) {
+    let pre = null;
+    let cur = head;
+    while (cur) {
+      const next = cur.next;
+      cur.next = pre;
+      pre = cur;
+      cur = next;
+    }
+    return pre;
+  }
+  let p = head;
+  let q = reverseList(slow);
+  while (q) {
+    if (p.val !== q.val) {
+      return false;
+    }
+    p = p.next;
+    q = q.next;
+  }
+  return true;
+};
 console.log(isPalindrome());
